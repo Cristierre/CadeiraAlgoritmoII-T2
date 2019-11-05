0df44f52581ece8c11f3a01c5ecca438f3571ce5
Cristierre <cristierrekonrath95@gmail.com>
2019-11-04 22:24:48 -0200

    cria lista candidatos

---
+++ Untitled2.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+struct candidato{
+	int numeroCanditado;
+	char nome[6];
+	char partido[3];
+	char cargo[1];
+	int numVotos;	
+};
+
+	struct candidato lista[11];
+
+	lista[0].numeroCanditado = 10;
+	lista[0].nome = "Jorge";
+	lista[0].partido = "ABC";
+	lista[0].cargo = "P";
+	
+	lista[1].numeroCanditado = 20;
+	lista[1].nome = "Marta";
+	lista[1].partido = "XYZ";
+	lista[1].cargo = "P";
+	
+	lista[2].numeroCanditado = 30;
+	lista[2].nome = "Sergio";
+	lista[2].partido = "DEF";
+	lista[2].cargo = "P";
+	
+	lista[3].numeroCanditado = 1001;
+	lista[3].nome = "Bruno";
+	lista[3].partido = "ABC";
+	lista[3].cargo = "D";
+	
+	lista[4].numeroCanditado = 1002;
+	lista[4].nome = "Bruna";
+	lista[4].partido = "ABC";
+	lista[4].cargo = "D";
+	
+	lista[5].numeroCanditado = 2001;
+	lista[5].nome = "Breno";
+	lista[5].partido = "XYZ";
+	lista[5].cargo = "D";
+	
+	lista[6].numeroCanditado = 2002;
+	lista[6].nome = "Bruno";
+	lista[6].partido = "XYZ";
+	lista[6].cargo = "D";
+	
+	lista[7].numeroCanditado = 2003;
+	lista[7].nome = "Nani";
+	lista[7].partido = "XYZ";
+	lista[7].cargo = "D";
+	
+	lista[8].numeroCanditado = 3001;
+	lista[8].nome = "Joao";
+	lista[8].partido = "DEF";
+	lista[8].cargo = "D";
+	
+	lista[9].numeroCanditado = 3002;
+	lista[9].nome = "Joana";
+	lista[9].partido = "DEF";
+	lista[9].cargo = "D";
+	
+	lista[10].numeroCanditado = 3003;
+	lista[10].nome = "Josmar";
+	lista[10].partido = "DEF";
+	lista[10].cargo = "D";
+	
+int main(){
+	
+}
+
+
+

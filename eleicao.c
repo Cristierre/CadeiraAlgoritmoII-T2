8d60dc0028e144a79fdd7db4abdb3b76304ad55f
Cristierre Konrath <cristierrekonrath95@gmail.com>
2019-11-05 13:54:02 -0300

    implementa funcao votacao

--- eleicao.c
+++ eleicao.c
@@ -1,89 +1,93 @@
 #include<stdio.h>
 struct candidato{
-	int numeroCanditado;
+	int numeroCandidato;
 	char nome[6];
 	char partido[3];
 	char cargo[1];
-	int numVotos;	
+	int numVotos;
 };
 
+
 	struct candidato lista[11];
+	float numVotos;
 
 
-	
+
 int main(){
-	lista[0].numeroCanditado = 10;
+	lista[0].numeroCandidato = 10;
 	strcpy(lista[0].nome, "Jorge");
 	strcpy(lista[0].partido, "ABC");
 	strcpy(lista[0].cargo, "P");
 	lista[0].numVotos = 0;
-	
-	lista[1].numeroCanditado = 20;
+
+	lista[1].numeroCandidato = 20;
 	strcpy(lista[1].nome, "Marta");
 	strcpy(lista[1].partido, "XYZ");
 	strcpy(lista[1].cargo, "P");
 	lista[1].numVotos = 0;
-	
-	lista[2].numeroCanditado = 30;
+
+	lista[2].numeroCandidato = 30;
 	strcpy(lista[2].nome, "Sergio");
 	strcpy(lista[2].partido, "DEF");
 	strcpy(lista[2].cargo, "P");
 	lista[2].numVotos = 0;
-	
-	lista[3].numeroCanditado = 1001;
+
+	lista[3].numeroCandidato = 1001;
 	strcpy(lista[3].nome, "Bruno");
 	strcpy(lista[3].partido, "ABC");
 	strcpy(lista[3].cargo, "D");
 	lista[3].numVotos = 0;
-	
-	lista[4].numeroCanditado = 1002;
+
+	lista[4].numeroCandidato = 1002;
     strcpy(lista[4].nome, "Bruna");
 	strcpy(lista[4].partido, "ABC");
 	strcpy(lista[4].cargo, "D");
 	lista[4].numVotos = 0;
-	
-	lista[5].numeroCanditado = 2001;
+
+	lista[5].numeroCandidato = 2001;
 	strcpy(lista[5].nome, "Breno");
 	strcpy(lista[5].partido, "XYZ");
 	strcpy(lista[5].cargo, "D");
 	lista[5].numVotos = 0;
-	
-	lista[6].numeroCanditado = 2002;
+
+	lista[6].numeroCandidato = 2002;
 	strcpy(lista[6].nome, "Bruno");
 	strcpy(lista[6].partido, "XYZ");
 	strcpy(lista[6].cargo, "D");
 	lista[6].numVotos = 0;
-	
-	lista[7].numeroCanditado = 2003;
+
+	lista[7].numeroCandidato = 2003;
 	strcpy(lista[7].nome, "Nani");
 	strcpy(lista[7].partido, "XYZ");
 	strcpy(lista[7].cargo, "D");
 	lista[7].numVotos = 0;
-	
-	lista[8].numeroCanditado = 3001;
+
+	lista[8].numeroCandidato = 3001;
 	strcpy(lista[8].nome, "Joao");
 	strcpy(lista[8].partido, "DEF");
 	strcpy(lista[8].cargo, "D");
 	lista[8].numVotos = 0;
-	
-	lista[9].numeroCanditado = 3002;
+
+	lista[9].numeroCandidato = 3002;
 	strcpy(lista[9].nome, "Joana");
 	strcpy(lista[9].partido, "DEF");
 	strcpy(lista[9].cargo, "D");
 	lista[9].numVotos = 0;
-	
-	lista[10].numeroCanditado = 3003;
+
+	lista[10].numeroCandidato = 3003;
 	strcpy(lista[10].nome, "Josmar");
 	strcpy(lista[10].partido, "DEF");
 	strcpy(lista[10].cargo, "D");
 	lista[10].numVotos = 0;
-	
+
 	menu();
+
 }
 
 int menu(){
-	int opc; 
-	
+	int opc;
+	int i;
+
 	printf("----------------- ELEICOES ----------------- \n\n");
 	printf("1- Votar \n");
 	printf("2- Lista de Candidatos Por Cargo \n");
@@ -91,10 +95,84 @@ int menu(){
 	printf("4- Apuracao Percentual de Votos");
 	printf("5- Quantidade Eleitos Por Partido Politico \n");
 	printf("6- Sair \n \n");
-	
+
 	printf("Digite a opcao desejada: ");
 	scanf("%d",&opc);
 	system("cls");
+
+	switch(opc){
+        case 1:
+            votacao();
+        break;
+	}
+	for(i = 0; i < 11; i++){
+        printf("Nome: %s,\n Numero de Votos: %d\n\n",lista[i].nome, lista[i].numVotos);
+    }
+}
+
+int votacao(){
+    int paraPresidente;
+    int paraDeputado;
+    struct candidato presidenteEscolhido;
+    struct candidato deputadoEscolhido;
+    int i;
+    int confirma = 0;
+
+    presidenteEscolhido.numeroCandidato = 0;
+    deputadoEscolhido.numeroCandidato = 0;
+
+    while(confirma == 0){
+
+        printf("-------- VOTO PARA PRESIDENTE -------\n \n");
+        printf("Digite o numero do candidato para presidencia: ");
+        scanf("%d", &paraPresidente);
+        printf("\n\n-------- VOTO PARA DEPUTADO  ------- \n \n");
+        printf("Digite o numero do candidato para deputado: ");
+        scanf("%d", &paraDeputado);
+        system("cls");
+
+        for(i = 0; i < 11; i++){
+            if(lista[i].numeroCandidato == paraPresidente){
+                presidenteEscolhido = lista[i];
+            }
+            if(lista[i].numeroCandidato == paraDeputado){
+                deputadoEscolhido = lista[i];
+            }
+        }
+        if(presidenteEscolhido.numeroCandidato != 0){
+            printf("--- PARA PRESIDENTE --- \n \n");
+            printf("Nome: %s \n\n", presidenteEscolhido.nome);
+            printf("Partido: %s \n\n", presidenteEscolhido.partido);
+
+        }else{
+            printf("Voce deve votar em um presidente! \n \n");
+        }
+        if(presidenteEscolhido.numeroCandidato != 0){
+            printf("--- PARA DEPUTADO --- \n \n");
+            printf("Nome: %s \n\n", deputadoEscolhido.nome);
+            printf("Partido: %s \n\n", deputadoEscolhido.partido);
+
+            printf("1 - CONFIRMA    0- CANCELA :");
+            scanf("%d", &confirma);
+
+            if(confirma == 1){
+                for(i = 0; i < 11; i++){
+                    if(lista[i].numeroCandidato == paraPresidente){
+                        lista[i].numVotos += 1;
+                    }
+                    if(lista[i].numeroCandidato == paraDeputado){
+                        lista[i].numVotos += 1;
+                    }
+                }
+            }
+        }else{
+            printf(" Voce deve votar em um deputado!");
+        }
+    }
+
+
+
+    return 0;
 }
 
 
